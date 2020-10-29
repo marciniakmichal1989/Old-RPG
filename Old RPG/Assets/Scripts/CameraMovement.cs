d9505fa00e37a461f44564ecb19b0a3d94c947c3
marciniakmichal1989 <marciniakmichal1989@gmail.com>
2020-10-29 17:37:25 +0100

    Update
    Update

---
+++ Old RPG/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform _target;
+    public float _smoothing;
+    public Vector2 _maxPosiotion;
+    public Vector2 _minPosiotion;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(_target.position.x,_target.position.y,transform.position.z);
+
+        targetPosition.x = Mathf.Clamp(_target.position.x, _minPosiotion.x,_maxPosiotion.x);
+        targetPosition.y = Mathf.Clamp(_target.position.y, _minPosiotion.y,_maxPosiotion.y);
+
+        if (transform.position != _target.position){
+                transform.position = Vector3.Lerp(transform.position,targetPosition,_smoothing);
+        }
+    }
+}
---
+++ Old RPG/Assets/Scripts/RoomMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 _cameraChange;
+    public Vector3 _playerChange;
+    private CameraMovement cam;
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("Player")){
+                cam._minPosiotion += _cameraChange;
+                cam._maxPosiotion += _cameraChange;
+                other.transform.position += _playerChange;
+        }
+
+    }
+    
+}
